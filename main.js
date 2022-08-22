8137efad8ae5157165311c61bf0474995622d9d3
LakshanyaPrabhu <88978072+Lakshanya-P@users.noreply.github.com>
2022-08-22 16:02:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,93 +1,22 @@
-var canvas= new fabric.Canvas("myCanvas");
-player_x=10;
-player_y=10;
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
 
-block_width=30;
-block_height=30;
-
-player_object="";
-block_object="";
-
-function player_update(){
-    fabric.Image.fromURL("player.png", function(Img){
-        player_object=Img;
-        player_object.scaleToWidth(150);
-        player_object.scaleToHeight(140);
-        player_object.set({
-            top:player_y,
-            left:player_x
-        });
-        canvas.add(player_object);        
-    });
-}
-
-function new_image(get_image){
-    fabric.Image.fromURL(get_image, function(Img){
+function new_image(){
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
         block_object=Img;
-        block_object.scaleToWidth(block_width);
-        block_object.scaleToHeight(block_height);
+        block_object.scaleToWidth(700);
+        block_object.scaleToHeight(510);
         block_object.set({
-            top:player_y,
-            left:player_x
+            top:100,
+            left:190,
+            right:190,
+            bottom:100
         });
         canvas.add(block_object);        
     });
 }
-window.addEventListener("keydown", my_keydown);
-function my_keydown(e){
-    keypressed = e.keyCode;
-    console.log(keypressed);
-    if(e.shiftKey == true && keypressed == '80')
-    {
-        block_width=block_width+10;
-        block_height=block_height+10;
-        document.getElementById("current_width").innerHTML=block_width;
-        document.getElementById("current_height").innerHTML=block_height;
-    }
-    if(e.shiftKey == true && keypressed == '77')
-    {
-        block_width=block_width-10;
-        block_height=block_height-10;
-        document.getElementById("current_width").innerHTML=block_width;
-        document.getElementById("current_height").innerHTML=block_height;
-    }
-    if(keypressed=="37"){
-        left();
-    }
-    if(keypressed=="38"){
-        up();
-    }
-    if(keypressed=="39"){
-        right();
-    }
-    if(keypressed=="40"){
-        down();
-    }
-    if(keypressed=="87"){
-        new_image("wall.jpg");
-    }
-    if(keypressed=="71"){
-        new_image("ground.png");
-    }
-    if(keypressed=="76"){
-        new_image("light_green.png");
-    }
-    if(keypressed=="84"){
-        new_image("trunk.jpg");
-    }
-    if(keypressed=="82"){
-        new_image("roof.jpg");
-    }
-    if(keypressed=="89"){
-        new_image("yellow_wall.png");
-    }
-    if(keypressed=="68"){
-        new_image("dark_green.png");
-    }
-    if(keypressed=="85"){
-        new_image("unique.png");
-    }
-    if(keypressed=="67"){
-        new_image("cloud.jpg");
-    }
+
+function play(){
+    x.play();
+	
 }
